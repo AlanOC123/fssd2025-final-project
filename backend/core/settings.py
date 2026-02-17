f4e5779adf0e227a4474d9cc6d4eb07fc0db4b0e
Alan O'Connor <aocthatsme246@gmail.com>
2026-02-17 22:35:33 +0000

    Added pre commit checks, set up automated testing and first test

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -8,13 +8,14 @@ from decouple import config
 def csv_list(v: str):
     """
     Splits a comma separated list into individual string values and returns them as an array
-    
+
     :param v: Commas separated list string
     :type v: str
     :return: String values of separated list
     :rtype: list[str]
     """
     return [s.strip() for s in v.split(",") if s.strip()]
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,8 +42,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 INSTALLED_APPS = [
     # Cloudinary Storage (Must be above static files)
-    "cloudinary_storage",           # Cloudinary Toolkit
-
+    "cloudinary_storage",  # Cloudinary Toolkit
     # Core Django Apps
     "django.contrib.admin",
     "django.contrib.auth",
@@ -51,35 +51,33 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
-
     # Third Party Libraries
-    "rest_framework",               # API Toolkit
-    "rest_framework_simplejwt",     # JSON Web Token Support
-    "rest_framework.authtoken",     # Token Authentication
-    "corsheaders",                  # Handles Cross-Origin Resource Sharing
-    "dj_rest_auth",                 # Auth Endpoints (Login, Logout, Password Reset)
-    "dj_rest_auth.registration",    # Auth Endpoints (Registration)
-    "allauth",                      # Identity Infrastructure
-    "allauth.account",              # Account Management
-    "allauth.socialaccount",        # Social Authentication (For Google Sign In?)
-    "anymail",                      # HTTP Email Tool
-    "cloudinary",                   # Cloudinary Main App Support
-
+    "rest_framework",  # API Toolkit
+    "rest_framework_simplejwt",  # JSON Web Token Support
+    "rest_framework.authtoken",  # Token Authentication
+    "corsheaders",  # Handles Cross-Origin Resource Sharing
+    "dj_rest_auth",  # Auth Endpoints (Login, Logout, Password Reset)
+    "dj_rest_auth.registration",  # Auth Endpoints (Registration)
+    "allauth",  # Identity Infrastructure
+    "allauth.account",  # Account Management
+    "allauth.socialaccount",  # Social Authentication (For Google Sign In?)
+    "anymail",  # HTTP Email Tool
+    "cloudinary",  # Cloudinary Main App Support
     # Apps
-    "apps.analytics",               # Analytics App
-    "apps.biology",                 # Biology App
-    "apps.energy",                  # Energy App
-    "apps.exercises",               # Exercises App
-    "apps.notifications",           # Notifications App
-    "apps.workouts",                # Workouts App
+    "core",  # Apex Model
+    "apps.analytics",  # Analytics App
+    "apps.biology",  # Biology App
+    "apps.energy",  # Energy App
+    "apps.exercises",  # Exercises App
+    "apps.notifications",  # Notifications App
+    "apps.workouts",  # Workouts App
 ]
 
-SITE_ID = 1 # Required for Site Identification and mapping by django.contrib.sites
+SITE_ID = 1  # Required for Site Identification and mapping by django.contrib.sites
 
 MIDDLEWARE = [
     # CORS Requirement to come top level for preflight requests
     "corsheaders.middleware.CorsMiddleware",
-
     # Base Django Middleware
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -88,12 +86,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-
     # Required Middleware by allauth
     "allauth.account.middleware.AccountMiddleware",
-
     # Whitenoise required for Cloudinary
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -141,73 +137,71 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
-    )
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
-AUTHENTICATION_BACKENDS =[
+AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend"
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 # --- CORS & CSRF (Frontend Integration)
-CSRF_COOKIE_HTTP_ONLY = False # Allows CSRF to be accessed by React if needed
+CSRF_COOKIE_HTTP_ONLY = False  # Allows CSRF to be accessed by React if needed
 CORS_ALLOW_CREDENTIALS = config("CORS_ALLOW_CREDENTIALS", default=True, cast=bool)
 
 # Whitelisted Origins that can make a request
-CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", default="http://localhost:5173", cast=csv_list)
-CORS_TRUSTED_ORIGINS = config("CORS_TRUSTED_ORIGINS", default="http://localhost:5173", cast=csv_list)
+CORS_ALLOWED_ORIGINS = config(
+    "CORS_ALLOWED_ORIGINS", default="http://localhost:5173", cast=csv_list
+)
+CORS_TRUSTED_ORIGINS = config(
+    "CORS_TRUSTED_ORIGINS", default="http://localhost:5173", cast=csv_list
+)
 
 # --- JWT Configuration
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),    # Short Lived Access Token
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),       # Long Lived Refresh Token
-    "ROTATE_REFRESH_TOKENS": False,                    # New refresh token on every use
-    "AUTH_HEADER_TYPES": ("Bearer",),                  # JWT Header
-    "BLACKLIST_AFTER_ROTATION": False,                 # Not Necessary
-    "REFRESH_TOKEN_LEEWAY": 20,                        # 20 Second Leeway to Account for Transit Time
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),  # Short Lived Access Token
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  # Long Lived Refresh Token
+    "ROTATE_REFRESH_TOKENS": False,  # New refresh token on every use
+    "AUTH_HEADER_TYPES": ("Bearer",),  # JWT Header
+    "BLACKLIST_AFTER_ROTATION": False,  # Not Necessary
+    "REFRESH_TOKEN_LEEWAY": 20,  # 20 Second Leeway to Account for Transit Time
 }
 
 # --- dj-rest-auth Configuration
 # Defines Security and Authentication Configuration for dj_rest_auth
 REST_AUTH = {
     # Serializers
-    'LOGIN_SERIALIZER': 'dj_rest_auth.serializers.LoginSerializer',
-    'TOKEN_SERIALIZER': 'dj_rest_auth.serializers.TokenSerializer',
-    'JWT_SERIALIZER': 'dj_rest_auth.serializers.JWTSerializer',
-    'JWT_SERIALIZER_WITH_EXPIRATION': 'dj_rest_auth.serializers.JWTSerializerWithExpiration',
-    'JWT_TOKEN_CLAIMS_SERIALIZER': 'rest_framework_simplejwt.serializers.TokenObtainPairSerializer',
-    'USER_DETAILS_SERIALIZER': 'dj_rest_auth.serializers.UserDetailsSerializer',
-    'PASSWORD_RESET_SERIALIZER': 'dj_rest_auth.serializers.PasswordResetSerializer',
-    'PASSWORD_RESET_CONFIRM_SERIALIZER': 'dj_rest_auth.serializers.PasswordResetConfirmSerializer',
-    'PASSWORD_CHANGE_SERIALIZER': 'dj_rest_auth.serializers.PasswordChangeSerializer',
-    'REGISTER_SERIALIZER': 'dj_rest_auth.registration.serializers.RegisterSerializer',
-
+    "LOGIN_SERIALIZER": "dj_rest_auth.serializers.LoginSerializer",
+    "TOKEN_SERIALIZER": "dj_rest_auth.serializers.TokenSerializer",
+    "JWT_SERIALIZER": "dj_rest_auth.serializers.JWTSerializer",
+    "JWT_SERIALIZER_WITH_EXPIRATION": "dj_rest_auth.serializers.JWTSerializerWithExpiration",
+    "JWT_TOKEN_CLAIMS_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "USER_DETAILS_SERIALIZER": "dj_rest_auth.serializers.UserDetailsSerializer",
+    "PASSWORD_RESET_SERIALIZER": "dj_rest_auth.serializers.PasswordResetSerializer",
+    "PASSWORD_RESET_CONFIRM_SERIALIZER": "dj_rest_auth.serializers.PasswordResetConfirmSerializer",
+    "PASSWORD_CHANGE_SERIALIZER": "dj_rest_auth.serializers.PasswordChangeSerializer",
+    "REGISTER_SERIALIZER": "dj_rest_auth.registration.serializers.RegisterSerializer",
     # Permission Classes
-    'REGISTER_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
-
+    "REGISTER_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     # JWT Creation Settings
-    'TOKEN_MODEL': 'rest_framework.authtoken.models.Token',
-    'TOKEN_CREATOR': 'dj_rest_auth.utils.default_create_token',
-
+    "TOKEN_MODEL": "rest_framework.authtoken.models.Token",
+    "TOKEN_CREATOR": "dj_rest_auth.utils.default_create_token",
     # Session Settings
-    'PASSWORD_RESET_USE_SITES_DOMAIN': False,
-    'OLD_PASSWORD_FIELD_ENABLED': False,
-    'LOGOUT_ON_PASSWORD_CHANGE': False,
-    'SESSION_LOGIN': False,
-    'USE_JWT': True,
-
+    "PASSWORD_RESET_USE_SITES_DOMAIN": False,
+    "OLD_PASSWORD_FIELD_ENABLED": False,
+    "LOGOUT_ON_PASSWORD_CHANGE": False,
+    "SESSION_LOGIN": False,
+    "USE_JWT": True,
     # JWT Role Settings
-    'JWT_AUTH_COOKIE': "auth_token",
-    'JWT_AUTH_REFRESH_COOKIE': "refresh_token",
-    'JWT_AUTH_REFRESH_COOKIE_PATH': '/',
-    'JWT_AUTH_SECURE': False,
-    'JWT_AUTH_HTTPONLY': True,
-    'JWT_AUTH_SAMESITE': 'None',
-    'JWT_AUTH_RETURN_EXPIRATION': True,
-    'JWT_AUTH_COOKIE_USE_CSRF': False,
-    'JWT_AUTH_COOKIE_ENFORCE_CSRF_ON_UNAUTHENTICATED': False,
+    "JWT_AUTH_COOKIE": "auth_token",
+    "JWT_AUTH_REFRESH_COOKIE": "refresh_token",
+    "JWT_AUTH_REFRESH_COOKIE_PATH": "/",
+    "JWT_AUTH_SECURE": False,
+    "JWT_AUTH_HTTPONLY": True,
+    "JWT_AUTH_SAMESITE": "None",
+    "JWT_AUTH_RETURN_EXPIRATION": True,
+    "JWT_AUTH_COOKIE_USE_CSRF": False,
+    "JWT_AUTH_COOKIE_ENFORCE_CSRF_ON_UNAUTHENTICATED": False,
 }
 
 # All Auth Configuration (Defaults for Now)
@@ -216,14 +210,16 @@ RATE_LIMITS = {
     "change_password": "5/m/user",
     "manage_email": "10/m/user",
     "reset_password": "20/min/ip,5/min/key",
-    "login_failed": "10/m/ip,5/5m/key"
+    "login_failed": "10/m/ip,5/5m/key",
 }
 
 # Use Email instead of Username for authentication
 ACCOUNT_LOGIN_METHODS = {"email"}
 
 ACCOUNT_SIGNUP_FIELDS = ["email*", "password1*", "password2*"]
-ACCOUNT_SIGNUP_FORM_HONEYPOT_FIELD = ["address",]
+ACCOUNT_SIGNUP_FORM_HONEYPOT_FIELD = [
+    "address",
+]
 
 ACCOUNT_LOGIN_FIELDS = {"email"}
 
@@ -252,9 +248,7 @@ if IS_PROD:
     # Email (Current Strategy is Anymail + Brevo HTTP Approach)
     EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
     DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
-    ANYMAIL = {
-        "BREVO_API_KEY": BREVO_API_KEY
-    }
+    ANYMAIL = {"BREVO_API_KEY": BREVO_API_KEY}
 else:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
@@ -278,8 +272,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 PASSWORD_RESET_LINK = config(
-    "PASSWORD_RESET_LINK",
-    default="http://localhost:5173/auth"
+    "PASSWORD_RESET_LINK", default="http://localhost:5173/auth"
 )
 
 # --- Storage Configuration ---
@@ -292,18 +285,17 @@ if IS_PROD:
     # Cloudinary - Production
     DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
     CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": config("CLOUD_NAME"),
-    "API_KEY": CLOUDINARY_API_KEY,
-    "API_SECRET": CLOUDINARY_API_SECRET_KEY
-}
+        "CLOUD_NAME": config("CLOUD_NAME"),
+        "API_KEY": CLOUDINARY_API_KEY,
+        "API_SECRET": CLOUDINARY_API_SECRET_KEY,
+    }
 else:
     # Django - Development
     DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
     MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "media" 
+    MEDIA_ROOT = BASE_DIR / "media"
 
 # Cloudinary Configuration
-
 
 # --- Internationalization & Static Files ---
 
